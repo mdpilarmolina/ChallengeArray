4de127fc4c6aa1a096ccf163207ac4a23b9854b5
mdpilarmolina <mdpilarmolina@gmail.com>
2026-07-01 13:37:10 -0300

    Consignas de cada ejercicio de Challenge 6 - practica de array

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,23 @@
-//1
+//1. Bucle del 1 al 20: Crea un bucle que imprima los números del 1 al 20.
 for (let i = 1; i <= 20; i++) {
   console.log(i);
 }
+
+//2. Tabla de multiplicar del 5: Escribe un bucle que imprima la tabla de multiplicar del 5.
+
+//3. Filtrar números mayores a 10. Dado el array: let numerosMayoresA10 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]; Usar un bucle para crear un nuevo array con solo los números mayores a 10.
+
+//4. Estación del año: Mostrar la estación correspondiente dependiendo del valor de la variable mes (Primavera, Otoño, Verano, Invierno).
+
+//5. Switch de idiomas: Usar un switch para imprimir un saludo diferente según el idioma (mínimo 7 idiomas).
+
+//6. Array de animales: Crear un array con 5 animales. Añadir 2 más (uno al inicio y otro al final). 
+// Eliminar el que está en la tercera posición.
+// Mostrar la cantidad total de animales.
+
+//7. Cálculo de IMC: Diseñar un algoritmo que calcule el IMC con peso (kg) y altura (m).
+// Mostrar el resultado con su categoría: Menos de 18.5 → Peso bajo. Entre 18.5 y 24.99 → Peso normal 25 o más → Sobrepeso
+// Fórmula: imc = peso / (altura ** 2);
 
 
 //arrayMundial
